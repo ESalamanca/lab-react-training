571e926bf06cb671fb3636c4ff3f27bb8e37af97
Elisa Salamanca <e_l_salamanca@yahoo.fr>
2020-02-08 17:03:44 +0100

    DriverCard

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,6 +5,7 @@ import Random from "./Random.js";
 import BoxColor from "./BoxColor.js";
 import CreditCard from "./CreditCard.js";
 import Rating from "./Rating.js";
+import DriverCard from "./DriverCard";
 
 class App extends Component {
   render() {
@@ -80,6 +81,26 @@ class App extends Component {
         <Rating>3</Rating>
         <Rating>4</Rating>
         <Rating>5</Rating>
+
+        <h1>Drivers Cards</h1>
+        <DriverCard
+          name="Travis Kalanick"
+          rating={4.2}
+          img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
+          car={{
+            model: "Toyota Corolla Altis",
+            licensePlate: "CO42DE"
+          }}
+        />
+        <DriverCard
+          name="Dara Khosrowshahi"
+          rating={4.9}
+          img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
+          car={{
+            model: "Audi A3",
+            licensePlate: "BE33ER"
+          }}
+        />
       </div>
     );
   }
